a4da3537ce72f69eab2c1516b93626740144699d
karamSH1 <sahawneh657@gamil.come.com>
2025-06-01 05:40:44 +0300

    Your descriptive commit message

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function toggleSidebar() {
     document.getElementById("sidebar").classList.toggle("active");
 }
+
+function shrinkOnClick(button) {
+  button.style.transform = "scale(0.95)";
+
+  // يرجع الحجم بعد 150 ملي ثانية
+  setTimeout(() => {
+    button.style.transform = "scale(1)";
+  }, 150);
+}
